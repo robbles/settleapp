918a8953317e5a6845965abf4af408d5078079a2
robbles <odwyerrob@gmail.com>
2012-11-11 00:03:27 -0800

    group routes and proper mongoose usage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
 
+var db = require('../database');
+
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+/*
+ * GET /groups
+ */
+exports.groups = function(req, res){
+  // TODO: filter by user
+  db.Group.find({})
+  .populate('owner')
+  .exec(function (err, groups) {
+    res.send(200, groups);
+  });
+};
+
+exports.group = function(req, res) {
+  var id = req.params.id;
+
+  // TODO: filter by user
+  db.Group.findOne({_id: id})
+  .populate('owner')
+  .populate('members')
+  .populate('expenses')
+  .exec(function (err, group) {
+    res.send(200, group);
+  });
 };
