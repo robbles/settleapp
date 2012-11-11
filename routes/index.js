7f2b99388bff207c4f62cfc5d6922415e94e406f
robbles <odwyerrob@gmail.com>
2012-11-11 12:13:01 -0800

    ensure login for index page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 
 var db = require('../database');
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
 /*
  * GET home page.
  */
-app.get('/', function(req, res){
-  console.log(req.user);
+app.get('/', 
+  ensureLoggedIn('/login'),
+  function(req, res) {
+  console.log('req.user: ' + (typeof req.user) + ' : ' + req.user);
+
   res.render('index', { title: 'Express', user: req.user });
 });
 
