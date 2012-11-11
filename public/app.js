d5de80781ca90fb8f2be3ea900498b754a9d32fc
andreimpop <andreimpop@gmail.com>
2012-11-11 14:10:24 -0800

    add groups from form - client side

--- public/app.js
+++ public/app.js
@@ -17,9 +17,9 @@ var AppRouter = Backbone.Router.extend({
   		email: "andreimpop@gmail.com"
   	}
 
-  	$('#contentBlock').fadeOut();
-	var settings = ich.settings(settings_data);
-	$('body').append(settings);
+	// $('#contentBlock').fadeOut();
+	// var settings = ich.settings(settings_data);
+	// $('body').append(settings);
 
  	}
 });
@@ -29,60 +29,107 @@ Backbone.history.start();
 
 $(document).ready(function () {
 
-  	var main = ich.main(App);
-	$('body').append(main);
+$('#nameGroup').click(function(){
+	return false;
+});
 
-		// adjust the modal for mobile devices
-			var adjustModal = function($modal) {
-			  var top;
-			  if ($(window).width() <= 480) {
-			    if ($modal.hasClass('modal-fullscreen')) {
-			      if ($modal.height() >= $(window).height()) {
-			        top = $(window).scrollTop();
-			      } else {
-			        top = $(window).scrollTop() + ($(window).height() - $modal.height()) / 2;
-			      }
-			    } else if ($modal.height() >= $(window).height() - 10) {
-			      top = $(window).scrollTop() + 10;
-			    } else {
-			      top = $(window).scrollTop() + ($(window).height() - $modal.height()) / 2;
-			    }
-			  } else {
-			    top = '50%';
-			    if ($modal.hasClass('modal-fullscreen')) {
-			      $modal.stop().animate({
-			          marginTop  : -($modal.outerHeight() / 2)
-			        , marginLeft : -($modal.outerWidth() / 2)
-			        , top        : top
-			      }, "fast");
-			      return;
-			    }
-			  }
-			  $modal.stop().animate({ 'top': top }, "fast");
-			};
+// add friends emails on enter
+$('#addFriends').live('keypress', function (e) {
+   if ( e.keyCode == 13 ){
+    	$('.invitedFriends').append("<li data-email="+$('#addFriends').val()+">"+$('#addFriends').val()+ " " + "<a class='removeFriend'>x</a></li>");
+    	$('#addFriends').val("");
+    } else {
+    	return true;
+    }
+    return false;
+});
 
-			var show = function() {
-			  var $modal = $(this);
-			  adjustModal($modal);
-			};
+$('.removeFriend').live("click", function() {
+	$(this).parent().remove();
+});
 
-			var checkShow = function() {
-			  $('.modal').each(function() {
-			    var $modal = $(this);
-			    if ($modal.css('display') !== 'block') return;
-			    adjustModal($modal);
-			  });
-			};
+$('#createGroup').live("click", function(){
+	var groupName = $("#groupName").val();
+	var emails = [];
+	$('.invitedFriends li').each(function(){
+   		emails.push($(this).data('email'));
+	});
+    
+	var groupInfo = {
+		groupName: groupName,
+		emails: emails
+	}
 
-			var modalWindowResize = function() {
-			  $('.modal').not('.modal-gallery').on('show', show);
-			  $('.modal-gallery').on('displayed', show);
-			  checkShow();
-			};
+	// send request to create group to server
+	$.ajax({
+	  type: 'POST',
+	  url: '/groups',
+	  data: groupInfo,
+	  dataType: 'json',
+	  succes: function(){console.log('create group success!')}
+	});
 
-			$(modalWindowResize);
-			$(window).resize(modalWindowResize);
-			$(window).scroll(checkShow);
+	$('#newGroupModal').modal('hide');
+	
+
+	return false;
+});
+
+// Insert global App into main body
+var main = ich.main(App);
+$('body').append(main);
+
+// adjust the modal for mobile devices
+var adjustModal = function($modal) {
+  var top;
+  if ($(window).width() <= 480) {
+    if ($modal.hasClass('modal-fullscreen')) {
+      if ($modal.height() >= $(window).height()) {
+        top = $(window).scrollTop();
+      } else {
+        top = $(window).scrollTop() + ($(window).height() - $modal.height()) / 2;
+      }
+    } else if ($modal.height() >= $(window).height() - 10) {
+      top = $(window).scrollTop() + 10;
+    } else {
+      top = $(window).scrollTop() + ($(window).height() - $modal.height()) / 2;
+    }
+  } else {
+    top = '50%';
+    if ($modal.hasClass('modal-fullscreen')) {
+      $modal.stop().animate({
+          marginTop  : -($modal.outerHeight() / 2)
+        , marginLeft : -($modal.outerWidth() / 2)
+        , top        : top
+      }, "fast");
+      return;
+    }
+  }
+  $modal.stop().animate({ 'top': top }, "fast");
+};
+
+var show = function() {
+  var $modal = $(this);
+  adjustModal($modal);
+};
+
+var checkShow = function() {
+  $('.modal').each(function() {
+    var $modal = $(this);
+    if ($modal.css('display') !== 'block') return;
+    adjustModal($modal);
+  });
+};
+
+var modalWindowResize = function() {
+  $('.modal').not('.modal-gallery').on('show', show);
+  $('.modal-gallery').on('displayed', show);
+  checkShow();
+};
+
+$(modalWindowResize);
+$(window).resize(modalWindowResize);
+$(window).scroll(checkShow);
 
 
 });
