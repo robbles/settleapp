51de62b2b851a17f3db8ea8e0f50b8f0ee31904f
Andrei Pop <andreimpop@gmail.com>
2012-11-13 11:54:54 -0800

    swapped out to social media icons

--- public/login.js
+++ public/login.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
 
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=200536266748566";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
 	$('body').append(ich.fbLogin());		
 
 	// handle facebook login click
